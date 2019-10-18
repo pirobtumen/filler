c8ab69960cc8f45fe2680ab554652b78bf55a1c0
a.sola <albsolac@gmail.com>
2019-10-18 23:11:37 +0200

    CSS Builder test

---
+++ test/unit/builder.css.test.ts
@@ -0,0 +1,22 @@
+jest.mock("uglifycss");
+import { processString } from "uglifycss";
+
+import { Store } from "../../src/lib/store";
+import { IFile } from "../../src/lib/interfaces";
+import { cssBuilder } from "../../src/lib/builder/builders";
+
+describe("Builder", () => {
+  test("CSS builder", async () => {
+    const store = new Store();
+    const fakeFile: IFile = {
+      name: "style.css",
+      extension: "css",
+      modifiedAt: new Date(),
+      path: "",
+      raw: "hello { fake-css: 1234; }"
+    };
+
+    const output = await cssBuilder(store, fakeFile);
+    expect(processString).toHaveBeenCalled();
+  });
+});
